fac011dc82170eed8ab53063f980ac765d6e22e1
chernishovasasha <chern.sh2005@gmail.com>
2024-10-14 18:00:59 +0300

    Добавьте файлы проекта.

---
+++ Employee.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Employee
+{
+protected:
+	string name;
+	float salary;
+public:
+	Employee();
+	Employee(string name, float salary);
+	string getName()const;
+	float getSalary()const;
+
+	void setName(string name);
+	void setSalary(float salary);
+
+	void showInfo()const;
+
+	float calcSalary()const;
+};
+
---
+++ T30_task1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "Employee.h"
+using namespace std;
+
+int main()
+{
+    Employee a("Brazhnyk Andriy", 15000);
+    a.showInfo();
+    cout << "Total salary: " << a.calcSalary() << endl;
+}
+
